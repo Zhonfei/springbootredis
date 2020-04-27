6c1f5f8c764446894e7b8d08bb218031b535307c
Zhonfei <46663664+Zhonfei@users.noreply.github.com>
2020-04-27 12:35:30 +0800

    spring boot整合Redis的Demo

---
+++ src/main/java/com/study/chapter0601/entity/Book.java
@@ -0,0 +1,47 @@
+package com.study.chapter0601.entity;
+
+import java.io.Serializable;
+
+/**
+ * Created by ZF on 2020/4/26.
+ */
+public class Book implements Serializable{
+    private Integer id;
+
+    private String name;
+
+    private String author;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
